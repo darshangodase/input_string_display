16ccf89c44f392aa69cfa6838f28d6ffddfb6a05
darshangodase <darshangodase10@gmail.com>
2024-02-25 12:08:57 +0530

    third commit

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const input=document.querySelector("input")
-const display=document.querySelector(".display")
-const button=document.querySelector("button")
+const input=document.querySelector("input");
+const display=document.querySelector(".display");
+const button=document.querySelector("button");
 
-button.addEventListener('click',dis)
-
-function dis()
+button.addEventListener('click',function dis()
 {
-  display.textContent = input.value
+  display.textContent = input.value ;
+  // display.innerHTML = input.value ;
 }
+);
+
 
 
